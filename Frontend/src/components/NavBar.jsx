b7eaef6c89af773abffeb499a69dd7201ea94aee
MehakBhutto <mehakbhutto842@gmail.com>
2026-06-21 23:06:40 +0500

    Update NavBar.jsx

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -34,6 +34,7 @@ const NavBar = () => {
             <li className="py-1">CONTACT</li>
             <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
+        <a target="_blank" onClick={()=>window.location.href ="https://prescripto-bfyq.vercel.app/"} className="border px-5 text-xs py-1.5 rounded-full">Admin Panel</a>
       </ul>
       <div className='flex items-center gap-4'>
         {
@@ -66,7 +67,6 @@ const NavBar = () => {
             <NavLink onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
             <NavLink onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
             <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
-            <a target="_blank" onClick={()=>window.location.href ="https://prescripto-bfyq.vercel.app/"} className="border px-5 text-xs py-1.5 rounded-full">Admin Panel</a>
           </ul>
         </div>
       </div>
